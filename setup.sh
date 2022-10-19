a5307ad4f384b42b0f22d93aa35f8a2569dcee3b
Akmal Soliev <akmal.soliev@nunatac.it>
2022-10-19 16:32:42 +0200

    Rebuilt the `setup.sh`, fixed the root issue
    Running `setup.sh` with sudo was giving an issue where all the actions
    were performed on the `root/` dir...
    
    Additionally, fixed all installation issues.

--- setup.sh
+++ setup.sh
@@ -1,74 +1,72 @@
 # Run this with super user administration
 SOURCE_DIR=$(pwd)
-cd $HOME
+export $(sudo env | grep SUDO)
+USER_DIR=/home/"$SUDO_USER"
+cd $USER_DIR
 
 ###################################################################################################
-# Installing git
-apt-get update 
-apt-get install git -y 
+# Installing latest version of git
+echo -ne '\n' | sudo apt-add-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git -y 
 
 ###################################################################################################
 # Docker
-apt-get remove docker docker-engine docker.io containerd runc -y
-apt-get update
-apt-get install \
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release -y 
 
-DIR=/etc/apt/keyrings
-if [ -d "$DIR" ];
-then
-    echo "$DIR directory exists."
-else
-    mkdir -p /etc/apt/keyrings 
-	echo "$DIR directory does not exist, creating it."
-fi
-
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-groupadd docker
-usermod -aG docker $USER
+
+sudo apt-get update
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 ###################################################################################################
 # Installing Vim from source
-apt install make -y
-apt-get install build-essential -y
-apt-get install libncurses5-dev libncursesw5-dev -y
-apt-get update 
+sudo apt install make -y
+sudo apt-get install build-essential -y
+sudo apt-get install libncurses5-dev libncursesw5-dev -y
+sudo apt-get update 
 git clone https://github.com/vim/vim.git
 cd vim/src
 ./configure
 ./make distclean
 ./make
 ./make install 
-cd $HOME
+cd $USER_DIR
 rm -rf vim
-# Plugin Manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-###################################################################################################
-# tmux 
-apt install tmux -y 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-###################################################################################################
-# Copying the configuration files to HOME dir
-cp "${SOURCE_DIR}"/config/. $HOME
-# Setting and installing plugins
-tmux source ~/.tmux.conf
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 ###################################################################################################
 # LSD Install
-apt-get install jq -y
+sudo apt-get install jq -y
 latestVersion=$(curl -sL https://api.github.com/repos/Peltoche/lsd/releases/latest | jq -r ".tag_name")
 fileName=lsd-musl_${latestVersion}_amd64.deb
 wget https://github.com/Peltoche/lsd/releases/download/${latestVersion}/${fileName}
-dpkg -i $fileName
+sudo dpkg -i $fileName
+rm $fileName
+
+###################################################################################################
+# tmux 
+sudo apt install tmux -y 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+###################################################################################################
+# Copying the configuration files to USER_DIR dir
+cp -r "${SOURCE_DIR}"/config/. $USER_DIR
+# tmux setting and installing plugins
+tmux source ~/.tmux.conf
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# Vim setting and installing plugins
+# Plugin Manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +'source ~/.vimrc' +'PlugInstall!' +'PlugUpdate!' +qa
